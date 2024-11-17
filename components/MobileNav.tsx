7104f15ead195303d4877f1d0ccdfc68d2cf06be
xHacker <115514882+athalbjb@users.noreply.github.com>
2024-11-16 21:00:22 -0800

    Update MobileNav.tsx

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,21 +1,12 @@
-"use client";
+'use client';
 
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetTrigger,
-} from "@/components/ui/sheet";
-import { sidebarLinks } from "@/constants";
-import { cn } from "@/lib/utils";
-import Image from "next/image";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { BiSolidSkipPreviousCircle } from "react-icons/bi";
-import { BsRecordCircleFill } from "react-icons/bs";
-import { FaUserLock } from "react-icons/fa";
-import { LuCalendarClock } from "react-icons/lu";
-import { MdDashboard } from "react-icons/md";
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { sidebarLinks } from '@/constants';
+import { cn } from '@/lib/utils';
 
 const MobileNav = () => {
   const pathname = usePathname();
@@ -35,46 +26,40 @@ const MobileNav = () => {
         <SheetContent side="left" className="border-none bg-dark-1">
           <Link href="/" className="flex items-center gap-1">
             <Image
-              className="w-[100px]"
-              src="/icons/logo.png"
+              src="/icons/logo.svg"
+              width={32}
+              height={32}
               alt="vertex logo"
-              width={200}
-              height={75}
             />
+            <p className="text-[26px] font-extrabold text-white">VERTEX</p>
           </Link>
-          <div className="h-[calc(100dvh-72px)] flex-between flex-col overflow-y-auto">
+          <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
             <SheetClose asChild>
-              <section className="h-full flex flex-col gap-6 pt-16 text-white">
-                {sidebarLinks.map((link) => {
-                  const isActive =
-                    pathname === link.route ||
-                    pathname.startsWith(`${link.route}/`);
+              <section className=" flex h-full flex-col gap-6 pt-16 text-white">
+                {sidebarLinks.map((item) => {
+                  const isActive = pathname === item.route;
+
                   return (
-                    <Link
-                      href={link.route}
-                      key={link.label}
-                      className={cn(
-                        "w-full max-w-64 p-4 px-10 flex items-center gap-4 rounded-lg hover:bg-green-2",
-                        {
-                          "bg-green-2": isActive,
-                        }
-                      )}
-                    >
-                      <span className="text-3xl text-sky-1">
-                        {link.id === "1" ? (
-                          <MdDashboard />
-                        ) : link.id === "2" ? (
-                          <LuCalendarClock />
-                        ) : link.id === "3" ? (
-                          <BiSolidSkipPreviousCircle />
-                        ) : link.id === "4" ? (
-                          <BsRecordCircleFill />
-                        ) : (
-                          <FaUserLock />
+                    <SheetClose asChild key={item.route}>
+                      <Link
+                        href={item.route}
+                        key={item.label}
+                        className={cn(
+                          'flex gap-4 items-center p-4 rounded-lg w-full max-w-60',
+                          {
+                            'bg-blue-1': isActive,
+                          }
                         )}
-                      </span>
-                      <span className="font-semibold">{link.label}</span>
-                    </Link>
+                      >
+                        <Image
+                          src={item.imgURL}
+                          alt={item.label}
+                          width={20}
+                          height={20}
+                        />
+                        <p className="font-semibold">{item.label}</p>
+                      </Link>
+                    </SheetClose>
                   );
                 })}
               </section>
